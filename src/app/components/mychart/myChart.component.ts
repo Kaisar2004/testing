adfa2902e59fbd1235711cae2e5dc8562ddf244a
Kaisar <nasipolla2019@mail.ru>
2024-09-30 20:12:25 +0500

    added 4 charts

--- src/app/components/mychart/myChart.component.ts
+++ src/app/components/mychart/myChart.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Chart, registerables} from "chart.js";
 import 'chartjs-adapter-date-fns';
 import {FormsModule} from "@angular/forms";
+
 Chart.register(...registerables);
 
 @Component({
@@ -14,15 +15,68 @@ Chart.register(...registerables);
   styleUrl: './myChart.component.css'
 })
 export class MyChartComponent {
-  private myChart: any = new Chart('myChart', {
+  private data = {
+    labels: [] as string[],     // Массив строк
+    datasets: [{
+      label: "Sensor",
+      data: [] as number[],     // Массив чисел
+    }]
+  };
+
+  private myChart1: Chart = new Chart('myChart1',{
     type: "line",
-    data: {
-      labels: [],
-      datasets: [{
-        label: 'Data',
-        data: [],
-      }]
-    },
+    data: this.data,
+    options: {
+      scales: {
+        x: {
+          type: 'time',
+          time: {
+            unit: 'day',
+          }
+        },
+        y: {
+          beginAtZero: true,
+        }
+      }
+    }
+  });
+  private myChart2: Chart = new Chart('myChart2',{
+    type: "line",
+    data: this.data,
+    options: {
+      scales: {
+        x: {
+          type: 'time',
+          time: {
+            unit: 'day',
+          }
+        },
+        y: {
+          beginAtZero: true,
+        }
+      }
+    }
+  });
+  private myChart3: Chart = new Chart('myChart3',{
+    type: "line",
+    data: this.data,
+    options: {
+      scales: {
+        x: {
+          type: 'time',
+          time: {
+            unit: 'day',
+          }
+        },
+        y: {
+          beginAtZero: true,
+        }
+      }
+    }
+  });
+  private myChart4: Chart = new Chart('myChart4',{
+    type: "line",
+    data: this.data,
     options: {
       scales: {
         x: {
@@ -38,12 +92,15 @@ export class MyChartComponent {
     }
   });
 
-  onSubmit(dataInput: string) {
+  setDataToChart(dataInput: string) {
     if (dataInput.length !== 0) {
-      const splitData = dataInput.split('-').join('');
-      this.myChart.config.data.labels.push(splitData);
-      this.myChart.config.data.datasets[0].data.push(Math.floor(Math.random() * 30))
-      this.myChart.update();
+      const splitData: string = dataInput.replace(/-/g, '');
+      const randomValue: number = Math.floor(Math.random() * 30);
+
+      this.data.labels.push(splitData);
+      // this.data.datasets[0].data.push(randomValue);
+      [this.myChart1, this.myChart2, this.myChart3, this.myChart4]?.find(item => item.data.datasets[0].data.push(randomValue));
+      [this.myChart1, this.myChart2, this.myChart3, this.myChart4].forEach(chart => chart.update());
     }
-  }
+  };
 }
