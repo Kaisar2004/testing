c5f3849aabc84eaf34d761ec0ae9ab8730c41022
Kaisar <nasipolla2019@mail.ru>
2024-10-01 00:22:03 +0500

    edited function setDataToChart, create new function for combinedatest

--- src/app/components/mychart/myChart.component.ts
+++ src/app/components/mychart/myChart.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Chart, registerables} from "chart.js";
 import 'chartjs-adapter-date-fns';
 import {FormsModule} from "@angular/forms";
@@ -15,17 +15,16 @@ Chart.register(...registerables);
   styleUrl: './myChart.component.css'
 })
 export class MyChartComponent {
-  private data = {
-    labels: [] as string[],     // Массив строк
-    datasets: [{
-      label: "Sensor",
-      data: [] as number[],     // Массив чисел
-    }]
-  };
 
-  private myChart1: Chart = new Chart('myChart1',{
+  private myChart1: Chart = new Chart('myChart1', {
     type: "line",
-    data: this.data,
+    data: {
+      labels: [] as string[],     // Массив строк
+      datasets: [{
+        label: "Sensor",
+        data: [] as number[],     // Массив чисел
+      }]
+    },
     options: {
       scales: {
         x: {
@@ -40,9 +39,15 @@ export class MyChartComponent {
       }
     }
   });
-  private myChart2: Chart = new Chart('myChart2',{
+  private myChart2: Chart = new Chart('myChart2', {
     type: "line",
-    data: this.data,
+    data: {
+      labels: [] as string[],     // Массив строк
+      datasets: [{
+        label: "Sensor",
+        data: [] as number[],     // Массив чисел
+      }]
+    },
     options: {
       scales: {
         x: {
@@ -57,9 +62,15 @@ export class MyChartComponent {
       }
     }
   });
-  private myChart3: Chart = new Chart('myChart3',{
+  private myChart3: Chart = new Chart('myChart3', {
     type: "line",
-    data: this.data,
+    data: {
+      labels: [] as string[],     // Массив строк
+      datasets: [{
+        label: "Sensor",
+        data: [] as number[],     // Массив чисел
+      }]
+    },
     options: {
       scales: {
         x: {
@@ -74,9 +85,15 @@ export class MyChartComponent {
       }
     }
   });
-  private myChart4: Chart = new Chart('myChart4',{
+  private myChart4: Chart = new Chart('myChart4', {
     type: "line",
-    data: this.data,
+    data: {
+      labels: [] as string[],     // Массив строк
+      datasets: [{
+        label: "Sensor",
+        data: [] as number[],     // Массив чисел
+      }]
+    },
     options: {
       scales: {
         x: {
@@ -95,12 +112,43 @@ export class MyChartComponent {
   setDataToChart(dataInput: string) {
     if (dataInput.length !== 0) {
       const splitData: string = dataInput.replace(/-/g, '');
-      const randomValue: number = Math.floor(Math.random() * 30);
+      const charts = [this.myChart1, this.myChart2, this.myChart3, this.myChart4];
 
-      this.data.labels.push(splitData);
-      // this.data.datasets[0].data.push(randomValue);
-      [this.myChart1, this.myChart2, this.myChart3, this.myChart4]?.find(item => item.data.datasets[0].data.push(randomValue));
-      [this.myChart1, this.myChart2, this.myChart3, this.myChart4].forEach(chart => chart.update());
+      charts.forEach(chart => {
+        const randomValue: number = Math.floor(Math.random() * 30);
+
+        chart?.data.datasets[0].data.push(randomValue);
+        chart?.data?.labels?.push(splitData);
+        chart?.update();
+      });
+    }
+  };
+
+  combineDataOfChart(dataInput: string) {
+    const combinedData = [...this.myChart1.config.data.datasets ?? [], ...this.myChart2.config.data.datasets ?? []];
+    const combinedLabels = [...this.myChart1.config.data.labels ?? [], ...this.myChart2.config.data.labels ?? []];
+
+    if (dataInput.length !== 0) {
+      const newChart = new Chart('myChart5', {
+        type: "line",
+        data: {
+          datasets: combinedData,
+          labels: combinedLabels,
+        },
+        options: {
+          scales: {
+            x: {
+              type: 'time',
+              time: {
+                unit: 'day',
+              }
+            },
+            y: {
+              beginAtZero: true,
+            }
+          }
+        },
+      });
     }
   };
 }
